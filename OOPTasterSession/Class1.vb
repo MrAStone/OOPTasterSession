d01a149a47127786c04ee99bcaeca31738ec3f69
A Stone <astone@heckgrammar.co.uk>
2022-12-07 11:18:53 +0000

    Taster Session Fo Y11

---
+++ OOPTasterSession/Class1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Class1
+
+End Class
+Public Class c
+    Public Shared Sub wl(Optional input = "")
+        Console.WriteLine(input)
+    End Sub
+    Public Shared Sub w(input)
+        Console.Write(input)
+    End Sub
+End Class
--- OOPTasterSession/Module1.vb
+++ OOPTasterSession/Module1.vb
@@ -1,7 +1,7 @@
 ﻿Module Module1
-
     Sub Main()
-
+        c.w("This is an example of me not having to write Console.write")
+        c.wl()
     End Sub
-
 End Module
+
